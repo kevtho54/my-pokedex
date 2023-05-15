6527047888020367035fa7aab8aa9f12b13f10de
kevtho54 <kevtho54@gmail.com>
2023-05-15 19:29:38 +0200

    modification du style pour la page fiche pokemon

--- pages/fiche_pokemon/_name.vue
+++ pages/fiche_pokemon/_name.vue
@@ -1,30 +1,68 @@
 <template>
   <div class="fiche-pokemon">
-   <nuxt-link to="/" class="btn">Retour</nuxt-link>
-    <h1>Fiche de {{ pokemonName }}</h1>
-    <img :src="pokemonImage" :alt="pokemonName">
-    <div class="description">
-      <p>pokémon n°{{pokemonOrder}}</p>
-      <p>pokémon de type {{ pokemonTypes }}</p>
-      <ul>
-        <li v-for="stat in pokemonStats">
-        {{ stat.name }}: {{ stat.base_stat }}
-      </li>
-      </ul>
-      <p> Poid : {{ pokemonWeight }} kg</p>
-    </div>
+  
+    <img class="logo" src="@/static/img/logo.png" alt="logo pokedex">
+    <div class="contener-sheet">
+      <h1 class="title">Fiche de {{ pokemonName }}</h1> 
+      <div class="contener-card">
+        <div class="description">
+          <p class="txt">pokémon n°{{pokemonOrder}}</p>
+          <p class="txt">pokémon de type {{ pokemonTypes }}</p>
+          <p class="txt"> Statistique du pokémon: </p>
+          <ul class="stats">
+            <li v-for="stat in pokemonStats">
+              {{ stat.name }}: {{ stat.base_stat }}
+            </li>
+          </ul>
+          <p> Poid : {{ pokemonWeight }} kg</p>
+        </div>
+
+        <!-- J'utilise une boucle v-for pour recupéré le background en fonction du type de pokemon. J'utilise ensuite la variable "btnColor" pour stocké la couleur et la réutilisé sur mon bouton retour -->
+
+        <div v-for="(type,index) in pokemon.types" :key="index" v-if="index ===0" :style="{ backgroundColor: btnColor=getTypeColor(pokemon.types[0].type.name)}" class="contener-img">
+          <img :src="pokemonImage" :alt="pokemonName" class="img-pokemon">
+        </div>
+      </div>
+      <nuxt-link to="/" class="btn-back rounded-pill text-center" :style="{backgroundColor: btnColor}">Retour au pokedex</nuxt-link>
+      
+  </div>
     
   </div>
 </template>
+
 <script>
+
+
 export default {
   data() {
     /* J'utilise la methode data() qui me retourne un objet avec comme propriété un tableau vide */
     return {
       pokemon: {},
+      btnColor: null,
+      typeColors: {
+        grass: "#67a300",
+        poison: "#c75db0",
+        fire: "#e68330",
+        flying: "#c9c8e8",
+        water: "#089bff",
+        bug: "#A8B820",
+        normal: "#A8A878",
+        electric:"#F8D030",
+        ground: "#E0C068",
+        fairy: "#EE99AC",
+        fighting: "#C03028",
+        psychic:"#F85888",
+        rock: "#B8A038",
+        steel : "#B8B8D0",
+        ice: "#98D8D8",
+        ghost : "#705898",
+        dragon : "#7038F8"
+      },
     };
   },
 
+  
+  
   async mounted() {
     console.log(this.team);
     try {
@@ -77,10 +115,16 @@ export default {
   },
   pokemonWeight() {
    return this.pokemon.weight
-  }
-
-
   },
+
+  
+},
+methods: {
+    getTypeColor(type){
+      return this.typeColors[type] || "gray"
+      },
+
+  }
   
   
 }
